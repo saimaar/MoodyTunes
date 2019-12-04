fcd82fd0d2b9fe3c06ed3484115d6cd03fcd4d55
Rex Ye <rxye1991@gmail.com>
2019-12-04 10:57:13 -0500

    finished matching songs with mood, then printed out the matching songs and their artists.

--- app/MoodyTune.rb
+++ app/MoodyTune.rb
@@ -10,27 +10,26 @@ class MoodyTune
     def initialize()
       @prompt = TTY::Prompt.new
 
-    # This is just reading @user in this class. 
+  # This is just reading @user in this class. 
   # here will be your CLI!
   # it is not an AR class so you need to add attr
     end
 
   def run
     # test 
+    # ask_mood_and_show_songs
     welcome
     login_signup
-    see_fav
+    show_favrouite_songs
  
     # get_joke(what_subject)
   end
-  
+
   private # Only getting called inside this class.
 
   # def test 
   #    prompt.select("Choose your destiny?", %w(Scorpion Kano Jax))
   # end 
-
-
   def welcome
       puts 'Welcome to MoodyTune!!! Full of music that will fit your mood.'
   end 
@@ -50,45 +49,60 @@ class MoodyTune
             favsong.user_id == @user.id 
       end 
   end 
+
+  def ask_mood_and_show_songs
+    
+    moods = ['Happy', 'Sad', 'Excited', 'Chill', 'Romantic']
+    mood_choice = prompt.select("What is your mood today?",moods)
+    matching_songs = Song.all.select do |each_song|
+           each_song.mood.downcase == mood_choice.downcase
+           
+     end    
+      
+    matching_songnames_artists =
+        matching_songs.map do |song|
+        # Returns song name and artist name. 
+        "#{song.songname} by #{song.artist}."
+    end 
+    display_songs(matching_songnames_artists)
+
  
-  def see_fav
+end  # End of ask_mood_and_show_songs
 
-
+def display_songs(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index+1}. #{song}"
+  end 
+end 
+    
+def show_favrouite_songs
+      fav_songs = fav_songs_instances 
       puts 'Would you like to see your favourite songs?'
       input = gets.chomp
 
-      if input.downcase == 'yes'
+      if input.downcase == 'yes' # and fav_songs is empty, puts 'no fav songs.'
         sleep(1)
         puts "\n"
-        puts 'Here are your favourite songs:'
+        
+        # If favrouite is empty , puts sorry message.
+         if fav_songs.empty?
+            puts "Sorry, your favourite list is empty. \n Lets add songs to the list."
+            ask_mood_and_show_songs
 
-         fav_songs = fav_songs_instances    
-        # binding.pry
+         else 
+        # If favrouite list is not empty, puts fav songs.
+          puts 'Here are your favourite songs:'
           fav_songs.map do |favsong|
-           # In order to get the songname, we need to go into the       
-           puts favsong.song.songname
-          # puts favsong.song.artist # Try to add artists here.
+            puts favsong.song.songname
           end 
-
-      elsif input.downcase == "no"
-          # Shows songs according to mood. 
-          show_songs_by_mood
-          
         end 
-      end 
-  end
-
-def show_songs_by_mood
-
-  moods = ['Happy😊', 'Sad🥺', 'Excited🤪', 'Chill😏', 'Romantic🥰']
-
-  
-
-
-
-  user_input = prompt.select("What is your mood?",moods)
-  puts user_input
- 
-
+      elsif input.downcase == 'no'
+        ask_mood_and_show_songs
+      else
+          puts 'Invalid input, please enter Yes or No.'
+          show_favrouite_songs
+    end 
+end
 end 
+
 
